1ad6f735e3c4d68eac06c56d35dfd2c6ba970b4c
Janek <jan.jankowski91@gmail.com>
2023-05-24 22:26:27 +0200

    init

---
+++ src/main/java/com/alpineguide/alpineguide/AlpineguideApplication.java
@@ -0,0 +1,13 @@
+package com.alpineguide.alpineguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpineguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlpineguideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpineguide/alpineguide/AlpineguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpineguide.alpineguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpineguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
